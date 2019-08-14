fd6a1a8c41d4cb920a96fd1f5c0eaacd30c4b8e6
mithunsmenon <mithunsmenon@gmail.com>
2019-08-14 20:24:15 +0530

    Use Image Instead Of Text In Nav Drawer

--- lib/main.dart
+++ lib/main.dart
@@ -111,10 +111,11 @@ class _MyHomePageState extends State<MyHomePage> {
           padding: EdgeInsets.zero,
           children: <Widget>[
             DrawerHeader(
-              child: Text('Kerala Rescue'),
+              child: null,
               decoration: BoxDecoration(
-                color: Colors.blue,
-              ),
+                  image: DecorationImage(
+                      image: AssetImage('assets/images/keralarescue.jpg'),
+                      fit: BoxFit.cover)),
             ),
             ListTile(
               title: Text('Home'),
