6d9949c9977bd0e7462d9efeaed10a42ffb02f91
ganeshsp1 <ganesh.sp006@gmail.com>
2019-08-17 12:47:17 +0530

    Contributor List updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'dart:io';
+// import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:keralarescue/annoucements.dart';
 import 'package:webview_flutter/webview_flutter.dart';
